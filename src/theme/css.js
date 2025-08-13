3d44a2a4a346a2008e0e3809dd3b8ea9d9c8f05e
Moaz Ateeq <moazateeq@gmail.com>
2025-08-13 18:10:24 +0500

    update yml file

--- src/theme/css.js
+++ src/theme/css.js
@@ -4,7 +4,7 @@ import { checkboxClasses } from '@mui/material/Checkbox';
 import { menuItemClasses } from '@mui/material/MenuItem';
 import { autocompleteClasses } from '@mui/material/Autocomplete';
 
-// ---------------------------------------------------------------updates for auto build-------
+// ---------------------------------------------------------------updates for auto build - test 2-------
 
 export const paper = ({ theme, bgcolor, dropdown }) => ({
   ...bgBlur({
